1923fcfcd331ec00cdbd6d84baf04fc559d8c546
barspinoz <109839725+barspinoz@users.noreply.github.com>
2026-02-13 00:01:11 -0600

    Add files via upload

--- EJ1.c
+++ EJ1.c
@@ -2,7 +2,7 @@
  * PRACTICA 1 de EDA I: ARREGLOS
  * El objetivo de este programa es comprender el uso
  * de los arreglos para almacenar y manipular información
- * contenida dentro de los arreglos.
+ * contenida.
  *
  * 12 de febrero del 2026
  *
@@ -17,20 +17,49 @@
 
 // Declaración de funciones a emplear
 void arreglos_enteros();
+void arreglos_flotantes();
+void arreglos_caracteres();
 
 // Función principal: main
-int main() {
-	printf("=== BIENVENIDO ===\n");
-
-	// EJEMPLO DE ARREGLOS ENTEROS
-	arreglos_enteros();
-
-	// EJEMPLO ARREGLOS FLOTANTES
-	arreglos_flotantes();
+void main() {
+	// Variables
+	int caso = 0;
 	
-	// EJEMPLO ARREGLOS CON CARACTERES
+	// Menu de seleccion	
+	do {
+		printf("=== BIENVENIDO ===\n");
+		printf("1. EJEMPLOS DE ARREGLOS CON ENTEROS\n");
+		printf("2. EJEMPLOS DE ARREGLOS CON FLOTANTES\n");
+		printf("3. EJEMPLOS DE ARREGLOS CON CARACTERES\n");
+		printf("4. SALIR\n");
+		printf("INGRESE SU OPCION: ");
+		scanf(" %d", &caso);	
 
-	return 0;
+		switch(caso) {
+			// ARREGLOS ENTEROS
+			case 1:
+				arreglos_enteros();
+				break;
+			// ARREGLOS FLOTANTES
+			case 2:
+				arreglos_flotantes();
+				break;
+			// ARREGLOS CON CARACTERES
+			case 3:
+				arreglos_caracteres();
+				break;
+			// SALIR
+			case 4:
+				printf("HASTA LUEGO\n");
+				break;
+
+			default:
+				printf("ERROR. VUELVA A INTENTAR\n");
+				break;
+		}
+
+	} while (caso != 4);
+
 }
 
 // Creación de las funciones a emplear
@@ -47,6 +76,7 @@ void arreglos_enteros() {
 	//
 	// Llenar arreglo
 	printf("=== EJEMPLO DE ARREGLO LINEAL ===\n");
+	printf("ARREGLO DE 1x7\n");
 	printf("INGRESE ENTEROS PARA LLENAR EL ARREGLO:\n");
 	
 	for(int i = 0; i < 7; i++) {
@@ -65,6 +95,7 @@ void arreglos_enteros() {
 	//
 	// Llenar arreglo
 	printf("\n=== EJEMPLO DE ARREGLO MATRICIAL ===\n");
+	printf("ARREGLO DE 2x5\n");
 	printf("INGRESE ENTEROS PARA LLENAR EL ARREGLO\n");
 	
 	for(int i = 0; i < 2; i++) {
@@ -88,6 +119,7 @@ void arreglos_enteros() {
 	//
 	// Llenar arreglo
 	printf("=== EJEMPLO DE ARREGLO TRIDIMENCIONAL ===\n");
+	printf("ARREGLO DE 3x3x3\n");
 	printf("INGRESE ENTEROS PARA LLENAR EL ARREGLO\n");
 
 	for(int i = 0; i < 3; i++) {
@@ -117,36 +149,38 @@ void arreglos_enteros() {
 void arreglos_flotantes() {
 	// Variables
 	float flotantes_uno[7] = {0}; // Arreglo Lineal
-	float flotantes_dos[2][5] = {0} // Arreglo Matricial
+	float flotantes_dos[2][5] = {0}; // Arreglo Matricial
 
 	// ARREGLO LINEAL
 	//
 	// Llenar arreglo
 	printf("=== EJEMPLO DE ARREGLO LINEAL ===\n");
+	printf("ARREGLO DE 1x7\n");
 	printf("INGRESE REALES PARA LLENAR EL ARREGLO:\n");
 
 	for(int i = 0; i < 7; i++) {
 		printf("\tArreglo[%d]: ", i);
-		scanf("%d", &flotantes_uno[i]);
+		scanf("%f", &flotantes_uno[i]);
 	}
 
 	// Mostrar el arreglo
 	printf("DATOS QUE SE INGRESARON:\n");
 	
 	for(int i = 0; i < 7; i++) {
-		printf("%d\t", flotantes[i]);
+		printf("%f\t", flotantes_uno[i]);
 	}
 
 	// ARREGLO MATRICIAL
 	//
 	// Llenar arreglo
 	printf("=== EJEMPLO DE ARREGLO MATRICIAL ===\n");
+	printf("ARREGLO DE 2x5");
 	printf("INGRESE REALES PARA LLENAR EL ARREGLO\n");
 
 	for(int i = 0; i < 2; i++) {
 		for (int j = 0; j < 5; j++) {
 			printf("\tArreglo[%d][%d]: ", i, j);
-			scanf("%d", &flotantes_dos[i][j]);
+			scanf("%f", &flotantes_dos[i][j]);
 		}
 	}
 
@@ -155,7 +189,57 @@ void arreglos_flotantes() {
 
 	for(int i = 0; i < 2; i++) {
 		for(int j = 0; j < 5; j++) {
-			printf("%d\t", flotantes_dos[i][j]);
+			printf("%f\t", flotantes_dos[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+// ARREGLOS CON CARACTERES
+void arreglos_caracteres() {
+	// Variables
+	char caracteres_uno[7] = {0};
+	char caracteres_dos[2][5] = {0};
+
+	// ARREGLO LINEAL
+	//
+	// Llenar arreglo
+	printf("=== EJEMPLO DE ARREGLO LINEAL ===\n");
+	printf("ARREGLO DE 1x7\n");
+	printf("Ingrese SOLAMENTE UN CARACTER para llenar el arreglo:\n");
+	
+	for(int i = 0; i < 7; i++) {
+		printf("\tArreglo[%d]: ", i);
+		scanf(" %c", &caracteres_uno[i]);
+	}
+
+	// Mostrar el arreglo
+	printf("DATOS QUE SE INGRESARON:\n");
+
+	for(int i = 0; i < 7; i++) {
+		printf("%c\t", caracteres_uno[i]);
+	}
+
+	// ARREGLO MATRICIAL
+	//
+	// Llenar arreglo
+	printf("\n=== EJEMPLO DE ARREGLO MATRICIAL ===\n");
+	printf("ARREGLO DE 2x5\n");
+	printf("Ingrese SOLAMENTE UN CARACTER para llenar el arreglo:\n");
+
+	for(int i = 0; i < 2; i++) {
+		for(int j = 0; j < 5; j++) {
+			printf("\tArreglo[%d][%d]: ", i, j);
+			scanf(" %c", &caracteres_dos[i][j]);
+		}
+	}
+
+	// Mostrar el arreglo
+	printf("DATOS QUE SE INGRESARON:\n");
+
+	for(int i = 0; i < 2; i++) {
+		for(int j = 0; j < 5; j++) {
+			printf("%c\t", caracteres_dos[i][j]);
 		}
 		printf("\n");
 	}
