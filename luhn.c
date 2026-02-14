88755c906d5b5f84d9d355df4284b757b2683b9a
barspinoz <109839725+barspinoz@users.noreply.github.com>
2026-02-14 15:55:44 -0600

    Add files via upload

---
+++ luhn.c
@@ -0,0 +1,21 @@
+/*
+ * ALGORITMO DE LUHN
+ *
+ * El algortimo de Luhn fue creado en 1954
+ * por Hans Peter Luhn. Se usa para la
+ * verificacion de numeros de tarjetas de
+ * credito.
+ *
+ * Roman Espinosa
+ *
+ * 14 febrero del 2026
+ *
+ */
+
+// Declaracion de las bibliotecas a emplear
+#include <stdio.h>
+
+// Declaracion de las funciones a emplear
+
+void main() {
+}
